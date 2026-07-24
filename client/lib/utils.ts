a47041267a6dccf31924fd518fa06b571073fb04
Kishore-Dev <kishoreprakash45@gmail.com>
2026-07-24 23:26:09 +0530

    Update utils.ts

--- client/lib/utils.ts
+++ client/lib/utils.ts
@@ -7,7 +7,7 @@ export function formatCurrency(amount: number): string {
 }
 
 export function cn(...classes: (string | boolean | undefined | null)[]): string {
-  return classes.filter(Boolean).join(" ");
+  
 }
 
 export function truncate(text: string, maxLength: number): string {
